fc5bd6ccec65bbb073d67ea2b3697b3c05cc2e0f
MarinaNguessan <marinanguessan23@gmail.com>
2023-03-02 12:06:07 +0000

    first commit

---
+++ src/exercice1/Main.java
@@ -0,0 +1,23 @@
+package exercice1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Créez l'arbre binaire d'exemple donné dans la question.
+	     Node root = new Node(26);
+	     root.left = new Node(10);
+	     root.right = new Node(3);
+	     root.left.left = new Node(4);
+	     root.left.right = new Node(6);
+	     root.right.right = new Node(3);
+
+	     // Vérifiez si l'arbre binaire est un SumTree ou non.
+	     SumTree sumTree = new SumTree();
+	     boolean isSumTree = sumTree.isSumTree(root);
+	     
+	     // Affichez le résultat.
+	     System.out.println(isSumTree);
+
+	}
+
+}
---
+++ src/exercice1/Node.java
@@ -0,0 +1,20 @@
+package exercice1;
+
+public class Node {
+	
+	int val;
+    Node left, right;
+    
+    Node(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+        
+     }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
